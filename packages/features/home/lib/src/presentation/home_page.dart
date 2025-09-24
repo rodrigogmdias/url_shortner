f76571430046305914e74a05a6fbcd2fab79c26e
rodrigogmdias <rodrigogmdias@gmail.com>
2025-09-23 23:03:48 -0300

    feat: refactor HomePage to StatefulWidget and enhance UI with URL input and list display

--- packages/features/home/lib/src/presentation/home_page.dart
+++ packages/features/home/lib/src/presentation/home_page.dart
@@ -1,10 +1,57 @@
+import 'package:design/design.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _urlController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home Page')));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: DesignTextField(
+                      urlController: _urlController,
+                      labelText: 'Enter URL',
+                    ),
+                  ),
+                  const SizedBox(width: 8.0),
+                  DesignButton(icon: Icons.send, onPressed: () {}),
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              DesignText('Recently Shortened URLs', type: DesignTextType.h3),
+              Expanded(
+                child: ListView.separated(
+                  itemCount: 15,
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, index) {
+                    return DesignListItem(
+                      title: 'Short URL $index',
+                      subtitle: 'https://short.url/$index',
+                      buttonIcon: Icons.copy,
+                      buttonOnPressed: () {},
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
